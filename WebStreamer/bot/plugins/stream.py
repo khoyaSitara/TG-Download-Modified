17386788fe5c7a55fe62cf711c7777e181fbf215
Khoya Sitara <99271761+khoyaSitara@users.noreply.github.com>
2023-11-18 05:24:06 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -28,20 +28,6 @@ from pyrogram.errors import UserNotParticipant
 )
 async def media_receive_handler(_, m: Message):
     banned_users = Var.BANNED_USERS.split() if Var.BANNED_USERS else []
-    if str(m.from_user.id) in banned_users:
-        return await m.reply("You are banned from using this bot.", quote=True)
-
-    try:
-        user = await _.get_chat_member(Var.UPDATES_CHANNEL_ID, user_id=m.from_user.id)
-    except UserNotParticipant:
-        return await m.reply(text="""<i>Join The Update Channel By Tapping The Join Now Button, To Use The Bot.</i>""",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("Join Now", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
-                        ]
-                    ],
-                ))
     if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS)):
         return await m.reply("You are not <b>allowed to use</b> Join @HashHackers to use me.", quote=True)
     try:
